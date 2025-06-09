b1556dce1abc29bb49fa9ef007edd83777b44a34
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-06-09 22:19:05 +0900

    init

---
+++ src/main/java/com/by/dallinday/DallindayApplication.java
@@ -0,0 +1,13 @@
+package com.by.dallinday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DallindayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DallindayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/by/dallinday/DallindayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.dallinday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DallindayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
