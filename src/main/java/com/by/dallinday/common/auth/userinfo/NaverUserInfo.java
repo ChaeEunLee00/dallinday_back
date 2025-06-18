496cc1395977ad626496f83a43d1b3792b068b82
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-06-18 17:37:27 +0900

    feat: 안쓰는 메서드 제거

--- src/main/java/com/by/dallinday/common/auth/userinfo/NaverUserInfo.java
+++ src/main/java/com/by/dallinday/common/auth/userinfo/NaverUserInfo.java
@@ -5,11 +5,11 @@ import java.util.Map;
 public class NaverUserInfo implements OAuth2UserInfo {
 
     private String provider;
-    private Map<String, Object> attributes; // getAttributes()
+    private Map<String, Object> response; // getAttributes()
 
     public NaverUserInfo(String provider, Map<String, Object> attributes) {
         this.provider = provider;
-        this.attributes = attributes;
+        this.response = (Map<String, Object>) attributes.get("response");
     }
 
     @Override
@@ -19,36 +19,20 @@ public class NaverUserInfo implements OAuth2UserInfo {
 
     @Override
     public String getProviderId() {
-        Map<String, Object> response = getStringObjectMap();
-        if (response == null) return null;
-
         return (String) response.get("id");
     }
     @Override
     public String getEmail() {
-        Map<String, Object> response = getStringObjectMap();
-        if (response == null) return null;
-
         return (String) response.get("email");
     }
 
     @Override
     public String getName() {
-        Map<String, Object> response = getStringObjectMap();
-        if (response == null) return null;
-
         return (String) response.get("name");
     }
 
     @Override
     public String getImageUrl() {
-        Map<String, Object> response = getStringObjectMap();
-        if (response == null) return null;
-
         return (String) response.get("profile_image");
-    }
-
-    private Map<String, Object> getStringObjectMap() {
-        return (Map<String, Object>) attributes.get("response");
     }
 }
