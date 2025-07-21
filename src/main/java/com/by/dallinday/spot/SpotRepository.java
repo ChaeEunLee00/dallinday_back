479774c490f23a378cc8fc2d1edcbe095dd63758
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-07-21 20:49:48 +0900

    feat: Spot 레파지토리 제거

--- src/main/java/com/by/dallinday/spot/SpotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.by.dallinday.spot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpotRepository extends JpaRepository<Spot, Long> {
-}
