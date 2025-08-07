ac38b58bf8ba93fb29cb4273362f97ce289a849f
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-08-07 19:55:54 +0900

    feat: 코스 예시 데이터 수정

--- src/main/java/com/by/dallinday/course/data/CourseDataInitializer.java
+++ src/main/java/com/by/dallinday/course/data/CourseDataInitializer.java
@@ -19,7 +19,7 @@ public class CourseDataInitializer implements CommandLineRunner {
         courseRepository.save(Course.of(
                 "COURSE001", "아산로 코스", 2.51, 16, 2,
                 "바다를 보며 걷기 좋은 산책로입니다.",
-                "인근에 해수욕장과 음식점이 있습니다.",
+                "장소1-장소2-장소3-장소4",
                 "https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/courses/%E1%84%8B%E1%85%A1%E1%84%89%E1%85%A1%E1%86%AB%E1%84%85%E1%85%A9.gpx",
                 "20250807120000", "20250807120000"
         ));
@@ -27,7 +27,7 @@ public class CourseDataInitializer implements CommandLineRunner {
         courseRepository.save(Course.of(
                 "COURSE002", "문수구장 코스", 2.7, 29, 1,
                 "숲속을 걷는 힐링 코스입니다.",
-                "휴게소와 주차장이 마련되어 있습니다.",
+                "장소1-장소2-장소3-장소4",
                 "https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/courses/%E1%84%86%E1%85%AE%E1%86%AB%E1%84%89%E1%85%AE%E1%84%80%E1%85%AE%E1%84%8C%E1%85%A1%E1%86%BC.gpx",
                 "20250807121000", "20250807121000"
         ));
@@ -35,7 +35,7 @@ public class CourseDataInitializer implements CommandLineRunner {
         courseRepository.save(Course.of(
                 "COURSE003", "울산대공원 코스", 2.6, 49, 1,
                 "도심 경관과 문화를 함께 즐길 수 있는 코스입니다.",
-                "주변에 박물관과 카페가 위치합니다.",
+                "장소1-장소2-장소3-장소4",
                 "https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/courses/%E1%84%8B%E1%85%AE%E1%86%AF%E1%84%89%E1%85%A1%E1%86%AB%E1%84%83%E1%85%A2%E1%84%80%E1%85%A9%E1%86%BC%E1%84%8B%E1%85%AF%E1%86%AB.gpx",
                 "20250807122000", "20250807122000"
         ));
@@ -43,7 +43,7 @@ public class CourseDataInitializer implements CommandLineRunner {
         courseRepository.save(Course.of(
                 "COURSE004", "태화강 코스", 40.2, 300, 3,
                 "경사가 있는 본격적인 산악 트레킹 코스입니다.",
-                "전망대와 쉼터가 곳곳에 있습니다.",
+                "장소1-장소2-장소3-장소4",
                 "https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/courses/%E1%84%90%E1%85%A2%E1%84%92%E1%85%AA%E1%84%80%E1%85%A1%E1%86%BC3%E1%84%89%E1%85%B5%E1%84%80%E1%85%A1%E1%86%AB.gpx",
                 "20250807123000", "20250807123000"
         ));
@@ -51,7 +51,7 @@ public class CourseDataInitializer implements CommandLineRunner {
         courseRepository.save(Course.of(
                 "COURSE005", "등대 코스", 1.35, 6, 2,
                 "강을 따라 자전거를 탈 수 있는 코스입니다.",
-                "대여소와 쉼터가 잘 갖춰져 있습니다.",
+                "장소1-장소2-장소3-장소4",
                 "https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/courses/%E1%84%83%E1%85%B3%E1%86%BC%E1%84%83%E1%85%A2.gpx",
                 "20250807124000", "20250807124000"
         ));
