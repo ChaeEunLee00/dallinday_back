fa4e9fc00eecb874644ef181895df3dde04a1906
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-08-20 18:14:03 +0900

    feat: 관리자 이름 변경

--- src/main/java/com/by/dallinday/common/Initializer/AdminDataInitializer.java
+++ src/main/java/com/by/dallinday/common/Initializer/AdminDataInitializer.java
@@ -35,7 +35,7 @@ public class AdminDataInitializer implements CommandLineRunner {
             admin.setProviderId(passwordEncoder.encode(adminPassword)); // 비밀번호 역할
             admin.setProvider("dallinday");
             admin.setRole(Role.ADMIN.getRole());
-            admin.setUsername("관리자");
+            admin.setUsername("달린데이");
             admin.setCreatedAt(LocalDateTime.now());
             admin.setImageUrl("https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/profile/default_profile.jpg");
 
