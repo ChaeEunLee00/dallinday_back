28545f5179b96b7d65520269bb5f306996a58f51
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-08-20 16:34:45 +0900

    feat: 관리자 이미지 주소 수정

--- src/main/java/com/by/dallinday/common/Initializer/AdminDataInitializer.java
+++ src/main/java/com/by/dallinday/common/Initializer/AdminDataInitializer.java
@@ -37,7 +37,7 @@ public class AdminDataInitializer implements CommandLineRunner {
             admin.setRole(Role.ADMIN.getRole());
             admin.setUsername("관리자");
             admin.setCreatedAt(LocalDateTime.now());
-            admin.setImageUrl("https://wanderworld-bucket.s3.ap-northeast-2.amazonaws.com/default_profile.jpg");
+            admin.setImageUrl("https://dallinday-bucket.s3.ap-northeast-2.amazonaws.com/profile/default_profile.jpg");
 
             memberRepository.save(admin);
             log.info("Admin user created.");
