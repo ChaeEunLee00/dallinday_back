bc5ecc3635406555bb03f2afd080b02dfcdf55f1
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-09-27 21:38:15 +0900

    feat: 테스트 제거

--- src/test/java/com/by/dallinday/course/CourseAPIClientTest.java
+++ src/test/java/com/by/dallinday/course/CourseAPIClientTest.java
@@ -18,19 +18,19 @@ class CourseAPIClientTest {
     @Autowired
     private CourseAPIClient courseApiClient;
 
-    @Test
-    void callCourseAPI_정상호출() {
-        // given & when
-        try {
-            List<CourseItem> courseItems = courseApiClient.callCourseAPI( 10, 1);
-
-            // then
-            assertThat(courseItems).isNotNull();
-            System.out.println("API 응답: " + courseItems);
-        } catch (BusinessLogicException e) {
-            fail("BusinessLogicException 발생: " + e.getMessage());
-        } catch (Exception e) {
-            fail("Exception 발생: " + e.getMessage());
-        }
-    }
+//    @Test
+//    void callCourseAPI_정상호출() {
+//        // given & when
+//        try {
+//            List<CourseItem> courseItems = courseApiClient.callCourseAPI( 10, 1);
+//
+//            // then
+//            assertThat(courseItems).isNotNull();
+//            System.out.println("API 응답: " + courseItems);
+//        } catch (BusinessLogicException e) {
+//            fail("BusinessLogicException 발생: " + e.getMessage());
+//        } catch (Exception e) {
+//            fail("Exception 발생: " + e.getMessage());
+//        }
+//    }
 }
